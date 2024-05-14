69e7a1fe7f34511186a2ae909ce0986fe969b3cf
wynn <wynn@Yonathans-MacBook-Pro.local>
2024-05-14 15:15:34 -0700

    update format change

--- app/api/removefile/route.ts
+++ app/api/removefile/route.ts
@@ -4,23 +4,25 @@ import { v2 as cloudinaryV2 } from "cloudinary";
 import { error } from "console";
 import ClientGenerator from "@/app/lib/contentful-client";
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const { spaceId } = data;
-  try {
+  const fileCollector = await req.json();
+  const { spaceId } = fileCollector;
+  console.log(fileCollector, "<-------starting, data")
+
     const client = await ClientGenerator();
     let filing = await client.getAsset(spaceId);
-
- const fileName = filing.fields.file?.fileName;
+  console.log("filing", filing);
+ const fileName = filing.fields.title;
 console.log(fileName, "fileName"),
-console.log(filing, "filing")
+console.log(filing, "<-------starting, filing")
+console.log(spaceId, "<-------starting, data")
+
     // Initialize cloudinary
     cloudinary.v2.config({
       cloud_name: "dev-wynn-las-vegas",
       api_key: "292399429862315",
       api_secret: "YjHv-NKNDtMqZTyRUkPdUWOZ-Tk",
     });
-    const data = await cloudinaryV2.uploader
-      .destroy(`visit-wynn/${fileName}`)
+    const data = await cloudinaryV2.uploader.destroy(`visit-wynn/${fileName}`)
       .then((result) => {
 console.log(result, "result")
         return result;
@@ -30,8 +32,4 @@ console.log(error, "result")
         return error;
       });
     return NextResponse.json({ message: true, status: 200 });
-
-  } catch (e) {
-    return NextResponse.json({ error:'internal Error', status: 500 });
-  }
 }
