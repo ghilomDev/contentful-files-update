c6086611fdbf2d0d571bb079f6f5854c8949c0fb
wynn <wynn@Yonathans-MacBook-Pro.local>
2024-05-14 15:52:14 -0700

    update format change

--- app/api/removefile/route.ts
+++ app/api/removefile/route.ts
@@ -5,31 +5,35 @@ import { error } from "console";
 import ClientGenerator from "@/app/lib/contentful-client";
 export async function POST(req: NextRequest) {
   const fileCollector = await req.json();
-  const { spaceId } = fileCollector;
   console.log(fileCollector, "<-------starting, data")
+  return NextResponse.json({ message: true, status: 200 ,datas: fileCollector });
+  // const fileCollector = await req.json();
+  // const { spaceId } = fileCollector;
+  // console.log(fileCollector, "<-------starting, data")
 
-    const client = await ClientGenerator();
-    let filing = await client.getAsset(spaceId);
-  console.log("filing", filing);
- const fileName = filing.fields.title;
-console.log(fileName, "fileName"),
-console.log(filing, "<-------starting, filing")
-console.log(spaceId, "<-------starting, data")
+//     const client = await ClientGenerator();
+//     let filing = await client.getAsset(spaceId);
+//   console.log("filing", filing);
+//  const fileName = filing.fields.title;
+// console.log(fileName, "fileName"),
+// console.log(filing, "<-------starting, filing")
+// console.log(spaceId, "<-------starting, data")
 
     // Initialize cloudinary
-    cloudinary.v2.config({
-      cloud_name: "dev-wynn-las-vegas",
-      api_key: "292399429862315",
-      api_secret: "YjHv-NKNDtMqZTyRUkPdUWOZ-Tk",
-    });
-    const data = await cloudinaryV2.uploader.destroy(`visit-wynn/${fileName}`)
-      .then((result) => {
-console.log(result, "result")
-        return result;
-      })
-      .catch((error) => {
-console.log(error, "result")
-        return error;
-      });
-    return NextResponse.json({ message: true, status: 200 });
+//     cloudinary.v2.config({
+//       cloud_name: "dev-wynn-las-vegas",
+//       api_key: "292399429862315",
+//       api_secret: "YjHv-NKNDtMqZTyRUkPdUWOZ-Tk",
+//     });
+//     const data = await cloudinaryV2.uploader.destroy(`visit-wynn/${fileName}`)
+//       .then((result) => {
+// console.log(result, "result")
+//         return result;
+//       })
+//       .catch((error) => {
+// console.log(error, "result")
+//         return error;
+//       });
+
+    // return NextResponse.json({ message: true, status: 200 });
 }
