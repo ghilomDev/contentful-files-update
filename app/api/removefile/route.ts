e9cfbe3ab4e86a5ae4cc54a0587d0e115276f766
wynn <wynn@Yonathans-MacBook-Pro.local>
2024-05-14 22:34:01 -0700

    update format change

--- app/api/removefile/route.ts
+++ app/api/removefile/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createClient } from 'contentful';
 import cloudinary from "cloudinary";
 import { v2 as cloudinaryV2 } from "cloudinary";
 import { error } from "console";
@@ -7,50 +8,34 @@ export async function POST(req: NextRequest) {
   const fileCollector = await req.json();
 
    const { spaceId } = fileCollector;
-  console.log(fileCollector, "<-------starting, data")
+  // console.log(fileCollector, "<-------starting, data")
   if (!fileCollector) {
     return NextResponse.json({ message: false, status: 400 });
   }
-  const client = await ClientGenerator();
-  let filing = await client.getAsset(spaceId);
-  if (!filing) {
+  const client = createClient({
+    space:"dk7sfup6zsex",
+    accessToken: "Bp5GzW9gdTzqQRTIROu4ZgNMPS72dlSWNuYGF8f6jMo",
+  });
+try {
+  const res = await client.getAsset(spaceId);
+
+  // console.log(res.sys, "<-------------res"); // Access the 'status' property from 'sys' object
+  if (!res) {
     return NextResponse.json({ message: false, status: 400 });
   }
-  const fileName = filing.fields.title;
+  const fileName = res.fields.title;
 
-
-  return NextResponse.json({ message: true, status: 200 ,datas:fileName  });
-
-
-
-
-  // const fileCollector = await req.json();
-  // const { spaceId } = fileCollector;
-  // console.log(fileCollector, "<-------starting, data")
-
-//     const client = await ClientGenerator();
-//     let filing = await client.getAsset(spaceId);
-//   console.log("filing", filing);
-//  const fileName = filing.fields.title;
-// console.log(fileName, "fileName"),
-// console.log(filing, "<-------starting, filing")
-// console.log(spaceId, "<-------starting, data")
-
-    // Initialize cloudinary
-//     cloudinary.v2.config({
-//       cloud_name: "dev-wynn-las-vegas",
-//       api_key: "292399429862315",
-//       api_secret: "YjHv-NKNDtMqZTyRUkPdUWOZ-Tk",
-//     });
-//     const data = await cloudinaryV2.uploader.destroy(`visit-wynn/${fileName}`)
-//       .then((result) => {
-// console.log(result, "result")
-//         return result;
-//       })
-//       .catch((error) => {
-// console.log(error, "result")
-//         return error;
-//       });
-
-    // return NextResponse.json({ message: true, status: 200 });
+  // Initialize cloudinary
+  cloudinary.v2.config({
+    cloud_name: "dev-wynn-las-vegas",
+    api_key: "292399429862315",
+    api_secret: "YjHv-NKNDtMqZTyRUkPdUWOZ-Tk",
+  });
+  const data = await cloudinaryV2.uploader.destroy(`visit-wynn/${fileName}`);
+  const result = await data;
+  return NextResponse.json({ message: true, status: 200, result: result });
+} catch (error) {
+  console.error(error);
+  return NextResponse.json({ message: error, status: 400 });
+}
 }
